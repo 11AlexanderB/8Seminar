3c36e374022e1184df3ce0ee841eabb80debe2b6
11AlexanderB <bezhenar93@internet.ru>
2022-09-18 23:17:49 +0300

    3D массив

---
+++ 4task/Program.cs
@@ -0,0 +1,51 @@
+﻿int[,,] FillMatrix(int page, int row, int col)
+{
+    int[,,] Matrix = new int[page, row, col];
+
+    for (int i = 0; i < page; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            for (int k = 0; k < col; k++)
+            {
+                Matrix[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+    return Matrix;
+}
+
+void PrintMatrix(int[,,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            for (int k = 0; k < table.GetLength(2); k++)
+            {
+                Console.Write($"{table[i, j, k]}=({i}, {j}, {k})  ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+try
+{
+    Console.Write("Введите кординату page: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите кординату row: ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите кординату col: ");
+    int z = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    int[,,] Matrix = FillMatrix(x, y, z);
+    PrintMatrix(Matrix);
+}
+
+catch
+{
+    Console.Write("Ошибка");
+}
